b147a568e6a213ab5ce2589619bbfb138a4edc9b
Roman Piliavskyi <chernosliv.billy@gmail.com>
2021-06-02 13:19:49 +0300

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by PiliavskyiMBP on 02.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
